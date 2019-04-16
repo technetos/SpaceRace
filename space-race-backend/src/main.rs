33c3535d5c1c8e57f3fd58345adbd16faf38f2f2
technetos <mrgould93@gmail.com>
2019-04-16 17:47:50 -0400

    Setup game environment served from backend and deployed to browser

--- space-race-backend/src/main.rs
+++ space-race-backend/src/main.rs
@@ -1,10 +1,45 @@
 #![feature(async_await, await_macro, futures_api)]
 
 use std::fs;
-use tide::{http::StatusCode, Context, EndpointResult};
+use tide::{
+    http::{response::Builder as ResponseBuilder, StatusCode},
+    Context, EndpointResult,
+};
 
+const INDEX_PATH: &'static str = "..";
 const MESH_PATH: &'static str = "../assets/meshes";
+const SCRIPT_PATH: &'static str = "../assets/scripts";
 
+/// Serve the main page
+async fn page<T>(_cx: Context<T>) -> EndpointResult {
+    let bytes =
+        fs::read(format!("{}/{}", INDEX_PATH, "index.html")).map_err(|_| StatusCode::NOT_FOUND)?;
+
+    Ok(ResponseBuilder::new()
+        .header("Content-Type", "text/html")
+        .status(StatusCode::OK)
+        .body(bytes.into())
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .into())
+}
+
+/// Serve script assets
+async fn scripts<T>(cx: Context<T>) -> EndpointResult {
+    let name: String = cx
+        .param("name")
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let bytes = fs::read(format!("{}/{}", SCRIPT_PATH, name)).map_err(|_| StatusCode::NOT_FOUND)?;
+
+    Ok(ResponseBuilder::new()
+        .header("Content-Type", "application/javascript")
+        .status(StatusCode::OK)
+        .body(bytes.into())
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .into())
+}
+
+/// Serve mesh assets
 async fn meshes<T>(cx: Context<T>) -> EndpointResult<Vec<u8>> {
     let name: String = cx
         .param("name")
@@ -17,6 +52,8 @@ async fn meshes<T>(cx: Context<T>) -> EndpointResult<Vec<u8>> {
 
 fn main() {
     let mut app = tide::App::new(());
+    app.at("/").get(page);
+    app.at("/scripts/:name").get(scripts);
     app.at("/meshes/:name").get(meshes);
     app.serve("127.0.0.1:8000").unwrap();
 }
